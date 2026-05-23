8b2a382c5a89023d2691e4127b7333af07beef94
Ostik <117195748+Ostik1220@users.noreply.github.com>
2026-05-23 15:52:02 +0300

    save, added persist

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,12 +1,56 @@
 // store.js: Точка збору редаксу.
-import { devToolsEnhancer } from "@redux-devtools/extension";
+// import { devToolsEnhancer } from "@redux-devtools/extension";
 import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers } from "redux";
 import { contactsReducer } from "./contacts/contactsSlice";
 import { filterReducer } from "./filter/filterSlice";
+import storage from "redux-persist/es/storage";
+
+import {
+  FLUSH,
+  PAUSE,
+  PERSIST,
+  persistReducer,
+  persistStore,
+  PURGE,
+  REGISTER,
+  REHYDRATE,
+} from "redux-persist";
+// import { getDefaultNormalizer } from "@testing-library/dom";
+
+// export const store = configureStore({
+//   reducer: {
+//     contacts: contactsReducer,
+//     filter: filterReducer,
+//   },
+// });
+
+const persistConfig = {
+  key: "contacts",
+  storage,
+  whitelist: ["contacts"]
+};
+
+console.log(persistConfig)
+
+const rootReducer = combineReducers({
+  contacts: contactsReducer,
+  filter: filterReducer,
+})
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
 
 export const store = configureStore({
-  reducer: {
-    contacts: contactsReducer,
-    filter: filterReducer,
-  },
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+                ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+
+      },
+    }),
 });
+
+console.log(store);
+
+export let persistor = persistStore(store)
